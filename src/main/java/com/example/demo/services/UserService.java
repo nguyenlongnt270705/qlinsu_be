c68ae43f632953e74badb0f66052264e84e868a9
Long <nguyenlongnt2707@gmail.com>
2025-06-14 21:51:50 +0700

    update UserService

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -7,7 +7,7 @@ import com.example.demo.repositories.UserRepository;
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
