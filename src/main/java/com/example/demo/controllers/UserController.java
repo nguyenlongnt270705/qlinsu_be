11f97ea8c28a6477df739fcf40f2a7f9a080cbd5
Long <nguyenlongnt2707@gmail.com>
2025-06-14 22:13:06 +0700

    update UserController and UserService

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,13 +1,28 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
 
 @Controller
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     // Get all users
-    public String getAllUser() {
-        return "All users";
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUser() {
+        return ResponseEntity.status(HttpStatus.OK).body(this.userService.fetchAllUser());
     }
 
     // Get user by ID
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,7 +1,10 @@
 package com.example.demo.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entities.User;
 import com.example.demo.repositories.UserRepository;
 
 @Service
@@ -11,5 +14,9 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public List<User> fetchAllUser() {
+        return userRepository.findAll();
     }
 }
