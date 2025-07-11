f8594fce8c1e8fd8fcfa5bce137d8c9780d24050
BIT230152 <BIT230152@st.cmc-u.edu.vn>
2025-07-11 08:11:39 +0700

    nguyenlong fix cors, securityConfig, securityUtil, custom entrypoint

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -9,12 +9,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
 @Data
 @NoArgsConstructor
+@Getter
+@Setter
 @AllArgsConstructor
 public class User {
 
